6fa84db8c059c1f8265e89a49052a74b2216fd52
Brandon Green <brandon@brandonleeproductions.com>
2018-05-03 20:40:45 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,6 @@
 # Build your say_hello method here
 
+#I'm not sure why this is passing local tests, but still throwing errors.
 
 def say_hello(language = "Ruby", "Kent Beck")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
