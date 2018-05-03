ddc00748d7bec0e563b671e1e454d5af127f2f80
Brandon Green <brandon@brandonleeproductions.com>
2018-05-03 20:38:27 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,6 @@
 # Build your say_hello method here
 
-def say_hello(name)
-  
+
   def greeting(name, language = "Ruby" )
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
